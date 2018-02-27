71fdfc4f49c33fc3584cb8f5e72596c553174e0e
KY <kevinxy00@gmail.com>
2018-02-27 15:01:57 -0500

    added rudimentary gauge but needs adapting to my data

--- static/index_js.js
+++ static/index_js.js
@@ -22,6 +22,8 @@ d3.json("/names", function(error, response){
     plot_pie(response[0]);
     // plot the first bubble chart
     plot_bubble_chart(response[0]);
+    // create the gauge
+    create_gauge(response[0]);
 }) 
 
 // set sample to whatever option is selected
@@ -212,3 +214,75 @@ function plot_bubble_chart(sample){
         }
     }); // END Plotly.d3.json for the samples url
 } //END bubble chart function
+
+
+// *** gauge function ***
+function create_gauge(sample){
+    var gauge_url = "/wfreq/" + sample;
+
+    d3.json(gauge_url, function(error, response){
+        // Enter a speed between 0 and 180
+        var level = response;
+
+        // Trig to calc meter point
+        var degrees = 180 - level,
+            radius = .5;
+        var radians = degrees * Math.PI / 180;
+        var x = radius * Math.cos(radians);
+        var y = radius * Math.sin(radians);
+
+        // Path: may have to change to create a better triangle
+        var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+            pathX = String(x),
+            space = ' ',
+            pathY = String(y),
+            pathEnd = ' Z';
+        var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+        var data = [{ type: 'scatter',
+            x: [0], y:[0],
+            marker: {size: 28, color:'850000'},
+            showlegend: false,
+            name: 'speed',
+            text: level,
+            hoverinfo: 'text+name'},
+            { values: [50/6, 50/6, 50/6, 50/6, 50/6, 50/6, 50],
+            rotation: 90,
+            text: ['TOO FAST!', 'Pretty Fast', 'Fast', 'Average',
+                    'Slow', 'Super Slow', ''],
+            textinfo: 'text',
+            textposition:'inside',
+            marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                                'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                                'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                                'rgba(255, 255, 255, 0)']},
+            labels: ['151-180', '121-150', '91-120', '61-90', '31-60', '0-30', ''],
+            hoverinfo: 'label',
+            hole: .5,
+            type: 'pie',
+            showlegend: false
+        }];
+
+        var layout = {
+            shapes:[{
+                type: 'path',
+                path: path,
+                fillcolor: '850000',
+                line: {
+                    color: '850000'
+                }
+            }],
+            title: 'Gauge', 
+            Speed: 0-100,
+            height: 700,
+            width: 700,
+            xaxis: {zeroline:false, showticklabels:false,
+                        showgrid: false, range: [-1, 1]},
+            yaxis: {zeroline:false, showticklabels:false,
+                    showgrid: false, range: [-1, 1]}
+        };
+
+        Plotly.newPlot('washing_freq_gauge', data, layout);
+
+    });
+} // End gauge function
